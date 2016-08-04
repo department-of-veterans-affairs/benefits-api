e44edf42f3c7acf0ed49ec1d2dd753af894c8546
Austin <austin@Austins-MacBook-Pro.local>
2016-08-04 15:03:32 -0700

    Add specs and other files

---
+++ spec/requests/claims_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Claims management", type: :request do
+
+  it "creates a Claim" do
+    post "/claims", params: { claim: { id: 1 } }
+    expect(response).to match_response_schema("claim")
+  end
+
+end
