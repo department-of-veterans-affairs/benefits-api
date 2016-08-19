2d575719d2d79a6dff7a820386748d3bd7d6621f
Carl Johnson <carl@adhocteam.us>
2016-08-19 12:24:56 -0400

    Initial commit for EVSS client code (#3)
    * Initial commit for EVSS client code
    
    * EVSS client: Fix method names
    
    * EVSS client: Add default_timeout 5 seconds
    
    * EVSS: Change to using faraday as http client library
    
    * EVSS: Make connection private, memoized
    
    * EVSS: Add stub tests

---
+++ lib/evss.rb
@@ -0,0 +1,37 @@
+require 'faraday'
+
+module EVSS
+
+  class ClaimsService
+
+    attr :base_url, :default_timeout
+
+    def initialize(vaafi_headers={})
+      # TODO: Get base URI from env
+      @base_url = 'http://csraciapp6.evss.srarad.com:7003/wss-claims-services-web-3.1/rest'
+      @headers = vaafi_headers
+      @default_timeout = 5  # seconds
+    end
+
+    def claims
+      conn.get 'vbaClaimStatusService/getOpenClaims'
+    end
+
+    def create_intent_to_file
+      conn.post 'claimServicesExternalService/listAllIntentToFile' do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.body = '{}'
+      end
+    end
+
+    private
+
+    def conn
+      @conn ||= Faraday.new(@base_url, headers: @headers) do |faraday|
+        faraday.options.timeout = @default_timeout
+        faraday.adapter  :httpclient
+      end
+    end
+
+  end
+end
