e1176c8105178dd974ffbd5966fb9e51428508a2
Austin <amartin857+github@gmail.com>
2016-08-23 15:23:56 -0700

    Use cassette in spec

--- spec/requests/claims_spec.rb
+++ spec/requests/claims_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Claims management", type: :request do
   it "lists all Claim" do
-    get "/claims"
-    expect(response).to match_response_schema("claims")
+    VCR.use_cassette("evss/claims/claims") do
+      get "/claims"
+      expect(response).to match_response_schema("claims")
+    end
   end
 end
