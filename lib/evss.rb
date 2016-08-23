06fa3ce51dd188f547b5bf09daa882d882848ba3
Carl Johnson <carl@adhocteam.us>
2016-08-23 16:35:18 -0400

    EVSS client tests: Add VCR for endpoint mocking (#4)
    * EVSS client tests: Add VCR for endpoint mocking
    
    * EVSS: Clean up Rubocop complaints
    
    * Fix remaining Rubocop complaints
    
    * Rename client_spec to evss_spec
    
    * Move evss_spec into the lib folder

--- lib/evss.rb
+++ lib/evss.rb
@@ -1,26 +1,24 @@
-require 'faraday'
+require "faraday"
 
 module EVSS
-
   class ClaimsService
+    attr_accessor :base_url, :default_timeout
 
-    attr :base_url, :default_timeout
-
-    def initialize(vaafi_headers={})
+    def initialize(vaafi_headers = {})
       # TODO: Get base URI from env
-      @base_url = 'http://csraciapp6.evss.srarad.com:7003/wss-claims-services-web-3.1/rest'
+      @base_url = "http://csraciapp6.evss.srarad.com:7003/wss-claims-services-web-3.1/rest"
       @headers = vaafi_headers
-      @default_timeout = 5  # seconds
+      @default_timeout = 5 # seconds
     end
 
     def claims
-      conn.get 'vbaClaimStatusService/getOpenClaims'
+      conn.get "vbaClaimStatusService/getOpenClaims"
     end
 
     def create_intent_to_file
-      conn.post 'claimServicesExternalService/listAllIntentToFile' do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = '{}'
+      conn.post "claimServicesExternalService/listAllIntentToFile" do |req|
+        req.headers["Content-Type"] = "application/json"
+        req.body = "{}"
       end
     end
 
@@ -29,9 +27,8 @@ module EVSS
     def conn
       @conn ||= Faraday.new(@base_url, headers: @headers) do |faraday|
         faraday.options.timeout = @default_timeout
-        faraday.adapter  :httpclient
+        faraday.adapter :httpclient
       end
     end
-
   end
 end
